b41c906285cc90cd462354f307fced80e227861b
scarlet31 <kruhft@gmail.com>
2023-07-19 12:55:21 -0600

    Update lisp.sh
    Fixed typos.  Added some stuff. Added signature.

--- lisp.sh
+++ lisp.sh
@@ -8,7 +8,7 @@ There is a lot to this file, although there are only 3 lines.
 
 The first is the 'shebang' line.  It says to run this program thought /bin/sh, aka 'shell script'.
 
-The sceone 'renames' sbcl, Steel Bank Common Lisp, to lisp.sh.  
+The script 'renames' sbcl, Steel Bank Common Lisp, to lisp.sh.  
 
 This is so your lisp contains the options for --eval and --load.  
 
@@ -18,4 +18,11 @@ So you are given  the option of going higher level and programming a 'wrapper'.
 
 That is what this is.  A 'wrapper' script.
 
-The last line is to return that there was no errors.  
+The last line is to return that there was no errors.
+
+And allows this comment.
+
+--
+Burton John Samograd
+burtonjohnsamograd@protonmail.com
+2023
